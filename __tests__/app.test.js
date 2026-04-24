718f2f0e7de65d17c5a429a9aee23d5290a210fe
PRIMACOM\rs-fedro <fedro@rintis.co.id>
2026-04-24 14:59:02 +0700

    Add CiCD Setting, Add Unit Testing

---
+++ __tests__/app.test.js
@@ -0,0 +1,11 @@
+const request = require("supertest");
+const app = require("../app");
+
+describe("Express API", () => {
+  it("should return 200 and hello world", async () => {
+    const res = await request(app).get("/");
+
+    expect(res.statusCode).toBe(200);
+    expect(res.body.message).toBe("Hello from Express CI/CD! Test Deployment");
+  });
+});
