38c9cf1f3aeb11ccee3f5cf93dde37aa74bab2fd
PRIMACOM\rs-fedro <fedro@rintis.co.id>
2026-04-28 08:31:27 +0700

    test rollback

--- index.js
+++ index.js
@@ -5,14 +5,6 @@ app.get("/", (req, res) => {
   res.json({ message: "Hello from Express CI/CD! Test Deployment" });
 });
 
-app.get("/test", (req, res) => {
-  res.json({ message: "Update Deployment" });
-});
-
-app.get("/test1", (req, res) => {
-  res.json({ message: "Horay New Update Available" });
-});
-
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
